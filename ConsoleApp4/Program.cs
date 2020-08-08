281676cc910846973d96d24afed1a16a15f3ad69
Vladimir Poleshchuk <polvlad1m@yandex.ru>
2020-08-08 16:58:55 +0700

    Изменил цикл

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -258,6 +258,17 @@ namespace ConsoleApp4
             Console.WriteLine(message);
 
 
+            for (int i = 0; i <5; i++)
+            {
+              
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("#");                                     
+                }
+                Console.WriteLine();
+            }
+
+
 
 
 
